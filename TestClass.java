0d8982f5a1c3fe4de69734f93db8f12ee07c30b5
cochraef <cochraef@rose-hulman.edu>
2019-11-05 13:34:47 -0500

    Testing Test

--- TestClass.java
+++ TestClass.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 public class TestClass {
 	public ArrayList<Integer> list;
 	public TestClass(){
-		this.list= new ArrayList<>();
+		this.list= new ArrayList<>(10000000);
 		System.out.print("Test");
+		System.out.println("HI");
 	}
 
 }
