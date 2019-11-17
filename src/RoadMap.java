fa918ee2eb3899f2480406678f282daf9044b29f
JrOtaku <emilya200.ea@gmail.com>
2019-11-17 01:41:44 -0500

    Added comments

--- src/RoadMap.java
+++ src/RoadMap.java
@@ -35,22 +35,24 @@ public class RoadMap<T extends Comparable<? super T>> {
 		referenceTable = new Hashtable<String, Node>();
 	}
 	
-	
+	//This method allows us to get the size of the map in other classes
 	public int getSize() {
 		return size;
 	}
 	
+	//This method adds a node to our graph
 	public boolean addNode(String name, NodeType type, double latitude, double longitude) {
 		referenceTable.put(name, new Node(name, type, latitude, longitude));
 		return true;
 	}
 	
-	
+	//This method adds an edge to our graph
 	public boolean addEdge(Node firstNode, Node secondNode, String name, EdgeType type, double distance) {
 		new Edge(firstNode, secondNode, name, type, distance);
 		return true;
 	}
 	
+	//This method gets an arraylist of all of the nodes in the graph
 	public ArrayList<Node> getAllCities() {
 		ArrayList<Node> a = new ArrayList<Node>();
 		
@@ -59,6 +61,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return a;
 	}
 
+	//This method gets an arraylist of all of the edges in the graph
 	public ArrayList<Edge> getAllEdges() {
 		ArrayList<Edge> edges = new ArrayList<Edge>();
 		TreeSet<Edge> e = new TreeSet<Edge>();
@@ -71,19 +74,26 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return edges;
 	}
 		
+	//This method allows us to find the node from string
+	//This method is used to convert the user input, a string, into something the code can use, a node.
 	public Node getNodeFromString(String key) {
 		Node value = referenceTable.get(key);
 		return value;
 	}
 	
+	//This method gets the distance of one road
 	public int getDistanceFromPath(ArrayList<Node> path) {
 		return pathLength(path, new WeightDistance());
 	}
 	
+	//This method gets the time it takes to travel down one road
 	public int getTimeFromPath(ArrayList<Node> path) {
 		return pathLength(path, new WeightTime());
 	}
 	
+	//This method searches through all of the locations to see if the input string is contained in any of the node titles
+	//It is case-sensitive
+	//This method gives the city searcher tab its functionality
 	public ArrayList<String> searchForCities(String input) {
 		ArrayList<String> matches = new ArrayList<String>();
 		
@@ -92,26 +102,34 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return matches;
 	}
 	
+	//This method finds the locations within a certain distance from the starting city
+	//This method allows the trip planner to calculate by distance
 	public ArrayList<ArrayList<Node>> getNearCitiesToDistance(Node startingCity, int distance) {
 		return getNearCities(startingCity, distance, new WeightDistance());
 	}
 	
+	//This method finds the locations that you can get to within a set amount of time
+	//The method allows the trip planner to calculate by time
 	public ArrayList<ArrayList<Node>> getNearCitiesToTime(Node startingCity, int time) {
 		return getNearCities(startingCity, time, new WeightTime());
 	}
 	
+	//This method finds the minimum distance between two nodes
 	public ArrayList<Node> findMinDistance(Node start, Node end) {
 		return AStar(start, end, new HeuristicDistance(), new WeightDistance(), -1);
 	}
 	
+	//This method finds the minimum time to get between two nodes
 	public ArrayList<Node> findMinTime(Node start, Node end) {
 		return AStar(start, end, new HeuristicTime(), new WeightTime(), -1);
 	}
 	
+	//This method find the minimum time between two nodes accounting for traffic
 	public ArrayList<Node> findMinTimeAccountingForTraffic(Node start, Node end, int startingTime) {
 		return AStar(start, end, new HeuristicTime(), new WeightTimeAccountingForTraffic(), startingTime);
 	}
 
+	//This method gets the between two nodes
 	private int pathLength(ArrayList<Node> path, LambdaW w) {
 		int length = 0;
 		for(int i = 0; i<path.size() - 1; i++) {
@@ -120,6 +138,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return length;
 	}
 	
+	//If two cities are adjacent, it returns the edge that connects them
 	private Edge getRoadBetweenCities(Node node, Node node2) {
 		int index = node.getConnectedCities().indexOf(node2);
 		if(index != -1) {
@@ -128,7 +147,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return null;
 	}
 
-
+	//This method implements the a-star algorithm 
 	private ArrayList<Node> AStar(Node start, Node end, LambdaH h, LambdaW w, int startingTime) {
 		
 		PriorityQueue<ComparableNode> queue = new PriorityQueue<ComparableNode>(); 
@@ -159,6 +178,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return queue.peek().currentPath;
 	}
 	
+	//This method takes in the starting city and the weight and an arraylist of nodes that are reachable
 	private ArrayList<ArrayList<Node>> getNearCities(Node startingCity, int distance, LambdaW w) {
 		
 		ArrayList<ArrayList<Node>> selectedNodes = new ArrayList<ArrayList<Node>>();
@@ -171,6 +191,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		return selectedNodes;
 	}
 	
+	//The method finds which cities are reachable
 	private void addCitiesToPath(ArrayList<Node> path, int currentDistance, int maxDistance, Node city, ArrayList<ArrayList<Node>> paths, LambdaW w) {
 		path.add(city);
 		if(currentDistance <= maxDistance) {
@@ -191,7 +212,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		}
 	}
 
-	private abstract class LambdaW { public abstract int weight(Edge e, int time); } // Jank
+	private abstract class LambdaW { public abstract int weight(Edge e, int time); }
 	
 	private class WeightDistance extends LambdaW { 
 		public int weight(Edge e, int time) { return (int) e.distance; } 
@@ -200,6 +221,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		public int weight(Edge e, int time) { return (int) e.time; } 
 	}
 	
+	//This method notes which times are rush hour and accounts for how much longer traffic will take
 	private class WeightTimeAccountingForTraffic extends LambdaW {
 		public int weight(Edge e, int time) { 
 			int t = (int) e.time;
@@ -247,6 +269,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		
 	private abstract class LambdaH { public abstract int heuristic(Node node1, Node node2); }
 	
+	//This calculates the distance from the latitude and longitude
 	private class HeuristicDistance extends LambdaH {
 		public int heuristic(Node node1, Node node2) {
 			if(node1 == null | node2 == null) throw new NullPointerException();
@@ -265,10 +288,11 @@ public class RoadMap<T extends Comparable<? super T>> {
 	
 	private class HeuristicTime extends LambdaH {
 		public int heuristic(Node node1, Node node2) {
-			return new HeuristicDistance().heuristic(node1, node2) * 60 / EdgeType.HIGHWAY.speedLimit; // TURBO-JANK (also convert to minutes)
+			return new HeuristicDistance().heuristic(node1, node2) * 60 / EdgeType.HIGHWAY.speedLimit;
 		}
 	}
-		
+
+	//This class creates the nodes
 	public class Node {
 		private ArrayList<Edge> connectedRoads;
 		
@@ -314,7 +338,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 		
 	}
 	
-	
+	//This class creates the edges needed to make the graph functional
 	public class Edge implements Comparable<Edge> {
 		private Node firstNode;
 		private Node secondNode;
@@ -324,9 +348,11 @@ public class RoadMap<T extends Comparable<? super T>> {
 		private double distance;
 		private double time;
 		
+		//These methods allow other classes to access the nodes related to the edge
 		public Node getFirstNode() { return firstNode; }
 		public Node getSecondNode() { return secondNode; }
 		
+		//This is the constructor, which creates the edge
 		public Edge(Node firstNode, Node secondNode, String name, EdgeType type, double distance) {
 			this.firstNode = firstNode;
 			this.secondNode = secondNode;
@@ -340,14 +366,18 @@ public class RoadMap<T extends Comparable<? super T>> {
 			this.time = this.distance / this.type.getSpeedLimit() * 60; // Minutes
 		}
 		
+		//This allows makes sure that we can access both nodes
 		public Node getOtherNode(Node node) {
 			if(firstNode.equals(node)) { return secondNode; }
 			return firstNode;
 		}
 		
+		//This method allows us to get the node as a string
 		public String toString() {
 			return name;
 		}
+		
+		//This method compares this edges to other edges
 		@Override
 		public int compareTo(Edge o) {
 			
@@ -359,13 +389,14 @@ public class RoadMap<T extends Comparable<? super T>> {
 		}
 	}
 	
-	
+	//This class creates the nodes needed to make the graph functional
 	private class ComparableNode implements Comparable<ComparableNode> {
 		ArrayList<Node> currentPath;
 		Integer cost;
 		Integer heuristic;
 		Integer currentTime;
 		
+		//The is the constructor, which creates the node given certain input
 		public ComparableNode(Node node, Integer cost, Integer heuristic, Integer startingTime) {
 			this.currentPath = new ArrayList<Node>();
 			currentPath.add(node);
@@ -374,7 +405,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 			this.currentTime = startingTime;
 		}
 		
-
+		//This construction makes the a comparable node given a list of nodes
 		public ComparableNode(ArrayList<Node> list, Integer cost, Integer heuristic, Integer newTime) {
 			this.currentPath = list;
 			this.cost = cost;
@@ -382,6 +413,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 			this.currentTime = newTime;
 		}
 		
+		//This creates a ArrayList of nodes
 		public ComparableNode createNewBranch(Node newCity, Integer cost, Integer heuristic, Integer newTime) {
 			ArrayList<Node> newList = new ArrayList<Node>();
 			newList.addAll(this.currentPath);
@@ -391,7 +423,7 @@ public class RoadMap<T extends Comparable<? super T>> {
 			return new ComparableNode(newList, newDistance, heuristic, newNewTime);
 		}
 		
-			
+		//This method compares two nodes
 		@Override
 		public int compareTo(ComparableNode n) {
 	        if(this.cost + this.heuristic > n.cost + n.heuristic) {
